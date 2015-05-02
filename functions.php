9e81f586c8110bc4dfa11c051117283f876dabd2
cbdweb <nik@cbdweb.net>
2015-05-03 09:52:16 +1000

    First commit

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}/* Adds the child theme setup function to the 'after_setup_theme' hook. */
+add_action( 'after_setup_theme', 'supernews_child_theme_setup', 11 );
+
+/**
+ * Setup function. All child themes should run their setup within this function. The idea is to add/remove 
+ * filters and actions after the parent theme has been set up. This function provides you that opportunity.
+ *
+ * @since 1.0
+ */
+function supernews_child_theme_setup() {
+	// Add your custom functions here.
+}
+
+if ( ! current_user_can( 'manage_options' ) ) {
+    show_admin_bar( false );
+}
