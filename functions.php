921ac5816eb8420ce38824f61e154bab802e95b3
cbdweb <nik@cbdweb.net>
2015-12-14 08:03:51 +1100

    add shortcode filter to text widget

--- functions.php
+++ functions.php
@@ -6,6 +6,9 @@ function theme_enqueue_styles() {
 }/* Adds the child theme setup function to the 'after_setup_theme' hook. */
 add_action( 'after_setup_theme', 'supernews_child_theme_setup', 11 );
 
+add_filter('widget_text', 'do_shortcode');
+
+
 /**
  * Setup function. All child themes should run their setup within this function. The idea is to add/remove 
  * filters and actions after the parent theme has been set up. This function provides you that opportunity.
