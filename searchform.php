406b3e51a617e58c9591c29d97ed2217d44e707e
cbdweb <nik@cbdweb.net>
2019-06-15 23:24:48 +1000

    override default searchform;

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="<?= esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text">Search for:</span>
+		<input type="search" class="search-field" placeholder="Search &hellip;" name="s" />
+	</label>
+	<input type="hidden" name="id" value="m" />
+</form>
