cb5b012c49e126bc382f40d3e43f9d72cd5c0f3f
nikdow <nik@cbdweb.net>
2023-01-02 19:08:13 +1100

    404 template handles private pages;

---
+++ 404.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Acme Themes
+ * @subpackage SuperNews
+ */
+get_header(); ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+      <section class="error-404 not-found">
+      <?php
+		$page_requested = get_page_by_path($_SERVER['REQUEST_URI']);
+		$page_status = get_post_status( $page_requested->ID );
+        $isPrivate = $page_status == 'private' && ! is_user_logged_in();
+      ?>
+        <header class="page-header">
+          <h1 class="page-title"><?php esc_html_e( $isPrivate ? 'This page requires a member login' : 'That page can&rsquo;t be found.', 'supernews' ); ?></h1>
+        </header><!-- .page-header -->
+
+        <div class="page-content">
+          <p><?php esc_html_e( $isPrivate ? 'Please login at left to view this page' : 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'supernews' ); ?></p>
+
+          <?php get_search_form(); ?>
+
+
+        </div><!-- .page-content -->
+      </section><!-- .error-404 -->
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+<?php get_footer();
